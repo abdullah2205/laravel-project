3577666a5f6fc236b1cb2987b296540ff3f602e5
Muhammad Abdullah <ma22052000@gmail.com>
2021-10-24 02:26:24 +0700

    fix tailwindcss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
